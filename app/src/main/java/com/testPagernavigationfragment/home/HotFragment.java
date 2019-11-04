0aa94fe5e70bf7080a08f844949b4081bd826639
kongming <kongming.yang@okcoin.com>
2019-11-04 14:13:11 +0800

    teacher first commit

--- app/src/main/java/com/testPagernavigationfragment/home/HotFragment.java
+++ app/src/main/java/com/testPagernavigationfragment/home/HotFragment.java
@@ -1,18 +1,21 @@
-package com.testPagernavigationfragment;
+package com.testPagernavigationfragment.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.testPagernavigationfragment.R;
 
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment_Info extends Fragment {
+public class HomeFragment_Hot extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.dic_layout_info, container, false);
+        View view = inflater.inflate(R.layout.dic_layout_hot, container, false);
         return view;
     }
 }
--- app/src/main/java/com/testPagernavigationfragment/home/InfoFragment.java
+++ app/src/main/java/com/testPagernavigationfragment/home/InfoFragment.java
@@ -1,19 +1,20 @@
-package com.testPagernavigationfragment;
+package com.testPagernavigationfragment.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import com.testPagernavigationfragment.R;
 
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment_Android extends Fragment {
+public class HomeFragment_Info extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.dic_layout_android, container, false);
+        View view = inflater.inflate(R.layout.dic_layout_info, container, false);
         return view;
     }
 }
